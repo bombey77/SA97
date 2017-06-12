716a18c66dee8d85752f1319b7c67391ea0f9ee5
Motovilov Roman <bombey131@gmail.com>
2017-06-12 22:17:15 +0200

    write this code myself

--- app/src/main/java/bombey77/sa97/MainActivity.java
+++ app/src/main/java/bombey77/sa97/MainActivity.java
@@ -4,9 +4,9 @@ package bombey77.sa97;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
@@ -14,55 +14,62 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String LOG = "myLogs";
 
-    ServiceConnection serviceConnection;
+    ServiceConnection serConn;
 
-    Intent intent = new Intent("bombey77.sa97service.bomba");
+    Intent intent;
 
-    private boolean bind;
+    boolean bind;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        intent = new Intent("bombey77.firstservicebinding.com");
         intent.setPackage("bombey77.servicesa97");
 
-        Log.d(LOG, "onCreate");
+        serConn = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                Log.d(LOG, "onServiceConnected");
+                bind = true;
+            }
 
-        serviceConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            bind = true;
-            Log.d(LOG, "onServiceConnected");
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            bind = false;
-            Log.d(LOG, "onServiceDisconnected");
-        }
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                Log.d(LOG, "onServiceDisconnected");
+                bind = false;
+            }
         };
     }
 
     public void onClickStartService(View view) {
         startService(intent);
-        Log.d(LOG, "onStartService");
+        Log.d(LOG, "onClickStartService");
     }
 
     public void onClickStopService(View view) {
         stopService(intent);
-        Log.d(LOG, "onStopService");
+        Log.d(LOG, "onClickStopService");
     }
 
     public void onClickBind(View view) {
-        bindService(intent, serviceConnection, BIND_AUTO_CREATE);
-        Log.d(LOG, "bindService");
+        bindService(intent, serConn, BIND_AUTO_CREATE);
+        bind = true;
+        Log.d(LOG, "onClickBind");
     }
 
     public void onClickUnbind(View view) {
-        if (!bind) return;
-        unbindService(serviceConnection);
+        if (!bind)return;
+        unbindService(serConn);
         bind = false;
-        Log.d(LOG, "onUnbindService ");
+        Log.d(LOG, "onClickUnbind");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG, "onDestroy - MainActivity");
+        onClickUnbind(null);
     }
 }
